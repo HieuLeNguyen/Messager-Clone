892f7740d6516b39c56aa9acf3ffc63ba74135dd
NguyenLeHieu <nguyenhieu157380@gmail.com>
2024-12-05 20:23:23 +0700

    Initial Commit

---
+++ Messager/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messager
+//
+//  Created by Nguyễn Văn Hiếu on 5/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
